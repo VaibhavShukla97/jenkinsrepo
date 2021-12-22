acf015ce00a14bc95871b9d8083025ee3dc848aa
VaibhavShukla97 <VaibhavShukla789@gmail.com>
2021-12-22 16:24:46 +0530

    2nd commit

---
+++ src/main/java/com/example/samplerest/TestController.java
@@ -0,0 +1,18 @@
+package com.example.samplerest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+@GetMapping("/hello-world")
+public Object helloWorld() {
+return "Hello World";
+}
+
+}
+
+
